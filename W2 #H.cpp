042eeae4c8e4ccc20d01947878d5a510b4ad6768
Moha665 <158146104+Mohannad-Elsayed@users.noreply.github.com>
2024-02-07 15:41:06 +0200

    Update W2 #H.cpp

--- W2 #H.cpp
+++ W2 #H.cpp
@@ -5,22 +5,21 @@ int main() {
 		ios::sync_with_stdio(0);
 		cin.tie(0);
 		
-	ll n, tmp, sum = 0, mx = LLONG_MIN; cin >> n;
-	set<ll> pfx;
-	while(n--){
-		cin >> tmp;
-		sum += tmp;
-		pfx.insert(sum);
-		if (tmp > mx) mx = tmp;
-		// cout << sum << ' ';
+	ll tn, n, tmp, sum = 0, mx = LLONG_MIN; 
+	cin >> n; tn = n;
+	vector<ll> input, pfx;
+	while(tn--){
+		cin >> tmp; input.push_back(tmp);
+		sum += tmp; pfx.push_back(sum);
+		mx = max({mx, sum, tmp});
 	}
-	if (pfx.size() == 1){
-		cout << *pfx.begin();
+	auto min_iterator = min_element(pfx.begin(), pfx.end());
+	if (*min_iterator == *pfx.rbegin()){
+		cout << mx; 
 		return 0;
 	}
-	for (ll x : pfx) cout << x << ' '; cout << endl;
-	// cout << ((*(pfx.rbegin()))) << ' ' << (*pfx.begin()) << endl;
-	ll fl = ((*(pfx.rbegin())) - (*pfx.begin()));
-	cout << max(fl, mx);
+	auto max_iterator = max_element(min_iterator+1, pfx.end());
+	ll val = *max_iterator - *min_iterator;
+	val > mx ? cout << val : cout << mx;
 	return 0;
 }
