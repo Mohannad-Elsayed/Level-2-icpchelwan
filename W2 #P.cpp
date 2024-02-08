3fed3cea70bc5756496bff0e111a336b6c598669
Moha665 <158146104+Mohannad-Elsayed@users.noreply.github.com>
2024-02-08 03:28:30 +0200

    Update W2 #P.cpp

--- W2 #P.cpp
+++ W2 #P.cpp
@@ -23,22 +23,48 @@ int main() {
 		cin >> p.first >> p.second;
 		v.push_back(p);
 	}
-	sort(v.rbegin(), v.rend());
-	for (int i = 0; i<v.size(); ++i){
-		ld dism = dis(m1, m2, v.at(i).first, v.at(i).second);
-		ld disz = dis(z1, z2, v.at(i).first, v.at(i).second);
-		if (dism > disz){
-			res += dism;
-			res += dis(b1, b2, v.at(i).first, v.at(i).second);
-			m1 = b1; m2 = b2;
-		}
-		else if (dism < disz){
-			res += disz;
-			res += dis(b1, b2, v.at(i).first, v.at(i).second);
-			z1 = b1; z2 = b2;
+	ld min_distance = LLONG_MAX, curr_distance; int i;
+	for (i = 0; i<v.size(); ++i){ // calculate the nearest bottle to mohannad
+		curr_distance = dis(z1, z2, v.at(i).first, v.at(i).second);
+		if (curr_distance < min_distance){
+			min_distance = curr_distance;
+			indx = i;
 		}
 	}
 	
+	// cout << "Min: " << min_distance << endl;
+	res += min_distance;
+	curr_distance = dis(v.at(indx).first, v.at(indx).second, b1, b2);
+	res += curr_distance;
+	v.erase(v.begin()+indx);
+	
+	if (!--n){ cout << fixed << setprecision(12) << res; return 0;}
+	
+	min_distance = LLONG_MAX;
+	for (i = 0; i<v.size(); ++i){ // calculate the nearest bottle to mazen
+		curr_distance = dis(m1, m2, v.at(i).first, v.at(i).second);
+		if (curr_distance < min_distance){
+			min_distance = curr_distance;
+			indx = i;
+		}
+	}
+	
+	res += min_distance;
+	curr_distance = dis(v.at(indx).first, v.at(indx).second, b1, b2);
+	cout << "Cur: " << curr_distance << endl;
+	res += curr_distance;
+	v.erase(v.begin()+indx);
+	
+	if (!--n){ cout << fixed << setprecision(12) << res; return 0;}
+	
+	cout << "Rer: " << res << endl;
+	for (i = 0; i<v.size(); ++i){ // calculate the remaining bottles for any of them, it doesn't matter
+		res += (2 * dis(b1, b2, v.at(i).first, v.at(i).second));
+		cout << v.at(i).first << ' ' <<  v.at(i).second << endl;
+
+	}
+	// cout << fixed << setprecision(12) << min_distance;
+	// cout << fixed << setprecision(12) << dis(0, 1, 1, 0);
 	cout << fixed << setprecision(12) << res << "  lol";
 	return 0;
 }
