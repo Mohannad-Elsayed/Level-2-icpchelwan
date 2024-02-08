227faee047617ff3af7e8d7ff59fe519881a2eb3
Moha665 <158146104+Mohannad-Elsayed@users.noreply.github.com>
2024-02-08 15:46:37 +0000

    Create --MyTemplate.cpp

---
+++ --MyTemplate.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+// #define int long long int
+#define FOR(i,a,b) for (int i=(int)(a);i<(int)(b);i++)
+#define ROF(i,a,b) for (int i=(int)(a);i>(int)(b);i--)
+#define all(x) (x).begin(),(x).end()
+#define ys cout << "YES";
+#define ns cout << "NO";
+#define ll long long
+#define ld long double
+#define llu long long unsigned
+#define si short int
+int tests(); int solve(){
+  //TODO tests()  solve() //
+    // !Start Here! */
+    
+  
+    // !Stop Here! */
+    return 0;
+}
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    return solve();
+    return tests();
+}
+int tests(){int t; cin >> t;while(t--){solve();cout << '\n';}return 0;}
