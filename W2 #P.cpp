37cf03f3b23e8327f7748eb7c0be301c3a6e6116
Moha665 <158146104+Mohannad-Elsayed@users.noreply.github.com>
2024-02-08 18:45:48 +0000

    Update W2 #P.cpp

--- W2 #P.cpp
+++ W2 #P.cpp
@@ -1,70 +1,74 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-#define ll long long int
+// #define int long long int
+#define FOR(i,a,b) for (int i=(int)(a);i<(int)(b);i++)
+#define ROF(i,a,b) for (int i=(int)(a);i>(int)(b);i--)
+#define all(x) (x).begin(),(x).end()
+#define ys cout << "YES";
+#define ns cout << "NO";
+#define ll long long
 #define ld long double
+#define llu long long unsigned
+#define si short int
 
+/*Calculate the distance*/
 ld dis(ld x1, ld y1, ld x2, ld y2){
-	return sqrt(pow(abs(x1-x2), 2) + pow(abs(y1-y2), 2));;
+	return sqrt(((x1-x2) * (x1-x2)) + ((y1-y2) * (y1-y2)));
 }
 
-int main() {
-		ios::sync_with_stdio(0);
-		cin.tie(0);
-	
-	ld res = 0;
-	ld m1, m2, z1, z2, b1, b2;
-	int n, indx;
-	vector<pair<ld, ld>> v;
-	cin >> m1 >> m2 
-	>> z1 >> z2 >> 
-	b1 >> b2 >> n;
-	for (int i = 0; i<n; ++i){
-		pair<ld, ld> p;
-		cin >> p.first >> p.second;
-		v.push_back(p);
-	}
-	ld min_distance = LLONG_MAX, curr_distance; int i;
-	for (i = 0; i<v.size(); ++i){ // calculate the nearest bottle to mohannad
-		curr_distance = dis(z1, z2, v.at(i).first, v.at(i).second);
-		if (curr_distance < min_distance){
-			min_distance = curr_distance;
-			indx = i;
-		}
-	}
-	
-	// cout << "Min: " << min_distance << endl;
-	res += min_distance;
-	curr_distance = dis(v.at(indx).first, v.at(indx).second, b1, b2);
-	res += curr_distance;
-	v.erase(v.begin()+indx);
-	
-	if (!--n){ cout << fixed << setprecision(12) << res; return 0;}
-	
-	min_distance = LLONG_MAX;
-	for (i = 0; i<v.size(); ++i){ // calculate the nearest bottle to mazen
-		curr_distance = dis(m1, m2, v.at(i).first, v.at(i).second);
-		if (curr_distance < min_distance){
-			min_distance = curr_distance;
-			indx = i;
-		}
-	}
-	
-	res += min_distance;
-	curr_distance = dis(v.at(indx).first, v.at(indx).second, b1, b2);
-	cout << "Cur: " << curr_distance << endl;
-	res += curr_distance;
-	v.erase(v.begin()+indx);
-	
-	if (!--n){ cout << fixed << setprecision(12) << res; return 0;}
-	
-	cout << "Rer: " << res << endl;
-	for (i = 0; i<v.size(); ++i){ // calculate the remaining bottles for any of them, it doesn't matter
-		res += (2 * dis(b1, b2, v.at(i).first, v.at(i).second));
-		cout << v.at(i).first << ' ' <<  v.at(i).second << endl;
+int tests(); int solve(){
+  //TODO tests()  solve() //
+    // !Start Here! */
+    ld stx, sty, ax, ay, bx, by, res = 0;
+    int n;
+    cin >> stx >> sty >> ax >> ay >> bx >> by >> n;
+    vector<pair<ld, ld>> v(n);
+    FOR(i, 0, n) cin >> v.at(i).first >> v.at(i).second;
 
-	}
-	// cout << fixed << setprecision(12) << min_distance;
-	// cout << fixed << setprecision(12) << dis(0, 1, 1, 0);
-	cout << fixed << setprecision(12) << res << "  lol";
-	return 0;
+    ld min_dis_st = LLONG_MAX, min_dis_a = LLONG_MAX, curr_dis_st, curr_dis_a;
+    int min_indx_st, min_indx_a;
+    FOR(i, 0, n){
+        ld xb = v.at(i).first, yb = v.at(i).second;
+        curr_dis_st = dis(xb, yb, stx, sty) + dis(bx, by, xb, yb);
+        curr_dis_a =  dis(xb, yb, ax, ay)   + dis(bx, by, xb, yb);
+        // cout << fixed << setprecision(12) << curr_dis_st << ' ' << curr_dis_a << ' ' << endl;
+        if (min_dis_a > curr_dis_a){
+            min_dis_a = curr_dis_a;
+            min_indx_a = i; 
+        }
+        {
+            if (min_dis_st > curr_dis_st){
+                min_dis_st = curr_dis_st;
+                min_indx_st = i;
+            }
+        }
+    }
+    if (min_dis_a < min_dis_st){ swap(stx, ax); swap(sty, ay); swap(min_dis_a, min_dis_st); swap(min_indx_a, min_indx_st);}
+
+    // cout << fixed << setprecision(12) << min_dis_st << ' ' << min_dis_a << ' ' << min_indx_st << ' ' << min_indx_a << endl << endl;
+
+    sort(v.rbegin(), v.rend());
+
+    res += min_dis_st; v.erase(v.begin() + min_indx_st);
+    FOR(i, 0, v.size()){
+        ld xb = v.at(i).first, yb = v.at(i).second;
+        curr_dis_st = 2 * dis(bx, by, xb, yb);
+        curr_dis_a  = dis(xb, yb, ax, ay) + dis(bx, by, xb, yb);
+        // cout << xb << ' ' << yb << "  ==  ";
+        // cout << fixed << setprecision(12) << curr_dis_a << ' ' << curr_dis_st << ' ' << endl;
+        if (curr_dis_a < curr_dis_st){
+            curr_dis_st = curr_dis_a; 
+            ax = bx; ay = by;
+        }
+         res += curr_dis_st;
+    }
+    cout << fixed << setprecision(12) << res;
+    // !Stop Here! */
+    return 0;
 }
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    return solve();
+    return tests();
+}
+int tests(){int t; cin >> t;while(t--){solve();cout << '\n';}return 0;}
