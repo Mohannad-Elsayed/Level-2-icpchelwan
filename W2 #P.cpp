0f12388393abe7d3f02f33925b5f13d0a8f8df10
Moha665 <158146104+Mohannad-Elsayed@users.noreply.github.com>
2024-02-08 22:41:21 +0000

    Update W2 #P.cpp

--- W2 #P.cpp
+++ W2 #P.cpp
@@ -19,50 +19,50 @@ ld dis(ld x1, ld y1, ld x2, ld y2){
 int tests(); int solve(){
   //TODO tests()  solve() //
     // !Start Here! */
-    ld stx, sty, ax, ay, bx, by, res = 0;
+    ld kx, ky, ax, ay, bx, by, res = 0;
     int n;
-    cin >> stx >> sty >> ax >> ay >> bx >> by >> n;
-    vector<pair<ld, ld>> v(n);
-    FOR(i, 0, n) cin >> v.at(i).first >> v.at(i).second;
-
-    ld min_dis_st = LLONG_MAX, min_dis_a = LLONG_MAX, curr_dis_st, curr_dis_a;
-    int min_indx_st, min_indx_a;
+    cin >> kx >> ky >> ax >> ay >> bx >> by >> n;
+    ld botx, boty; ld bb[n], ab[n], kb[n];
+    vector<vector<ld>> v(n);
     FOR(i, 0, n){
-        ld xb = v.at(i).first, yb = v.at(i).second;
-        curr_dis_st = dis(xb, yb, stx, sty) + dis(bx, by, xb, yb);
-        curr_dis_a =  dis(xb, yb, ax, ay)   + dis(bx, by, xb, yb);
-        // cout << fixed << setprecision(12) << curr_dis_st << ' ' << curr_dis_a << ' ' << endl;
-        if (min_dis_a > curr_dis_a){
-            min_dis_a = curr_dis_a;
-            min_indx_a = i; 
-        }
-        {
-            if (min_dis_st > curr_dis_st){
-                min_dis_st = curr_dis_st;
-                min_indx_st = i;
-            }
-        }
+        cin >> botx >> boty;
+        bb[i] = dis(botx, boty, bx, by);
+        kb[i] = dis(botx, boty, kx, ky);
+        ab[i] = dis(botx, boty, ax, ay);
+        v.at(i).push_back(bb[i]);
+        v.at(i).push_back(kb[i]);
+        v.at(i).push_back(ab[i]);
     }
-    if (min_dis_a < min_dis_st){ swap(stx, ax); swap(sty, ay); swap(min_dis_a, min_dis_st); swap(min_indx_a, min_indx_st);}
+    sort(v.begin(), v.end(), greater<vector<ld>>());
+    bool kk = false, aa = false;
 
-    // cout << fixed << setprecision(12) << min_dis_st << ' ' << min_dis_a << ' ' << min_indx_st << ' ' << min_indx_a << endl << endl;
-
-    sort(v.rbegin(), v.rend());
-
-    res += min_dis_st; v.erase(v.begin() + min_indx_st);
-    FOR(i, 0, v.size()){
-        ld xb = v.at(i).first, yb = v.at(i).second;
-        curr_dis_st = 2 * dis(bx, by, xb, yb);
-        curr_dis_a  = dis(xb, yb, ax, ay) + dis(bx, by, xb, yb);
-        // cout << xb << ' ' << yb << "  ==  ";
-        // cout << fixed << setprecision(12) << curr_dis_a << ' ' << curr_dis_st << ' ' << endl;
-        if (curr_dis_a < curr_dis_st){
-            curr_dis_st = curr_dis_a; 
-            ax = bx; ay = by;
+    FOR(i, 0, n){
+        res += v[i][0];
+        ld mn = min(v[i][2] , v[i][1]);
+        if (mn == v[i][2] && aa) mn = v[i][1];
+        else if (mn == v[i][1] && kk) mn = v[i][2];
+        if (mn > v[i][0] && (aa || kk)){
+            res += v[i][0];
         }
-         res += curr_dis_st;
+        else if (i==n-1 && (!aa && !kk)){
+            res += mn;
+        }
+
+        else if (mn == v[i][1] && !kk){
+            res += mn;
+            kk = true;
+        }
+        else if (mn == v[i][2] && !aa){
+            res += mn;
+            aa = true;
+            // cout << v[i][2] << " aa ";
+        }
+        else{
+            res += v[i][0];
+        }
     }
     cout << fixed << setprecision(12) << res;
+
     // !Stop Here! */
     return 0;
 }
