a60b5e6f71982fdeaa21dcfa03909bd9a1a07d07
Moha665 <158146104+Mohannad-Elsayed@users.noreply.github.com>
2024-02-10 03:23:46 +0200

    U ACC.cpp

--- W2 #U.cpp
+++ W2 #U.cpp
@@ -1,38 +1,70 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define ll long long int
-#define ld long double
+// Problem: U - Zular Eclipse
+// Contest: Virtual Judge - Week #2
+// URL: https://vjudge.net/contest/608666#problem/U
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+// By: Sakura Yamauchi
+// 
+// Powered by CP Editor (https://cpeditor.org)
 
-int main() {ios::sync_with_stdio(0);
-			cin.tie(0);
-			
-	map<int, pair<int, ll>> mp;
-	ll n, q; cin >> n; ll arr[n];
-	for (int i = 0; i<n; ++i) cin >> arr[i];
-	cin >> q; ll que[q];
-	for (int i = 0; i<n; ++i) cin >> que[i];
-	
-	sort(arr, arr+n);
-	ll sum = 0, curr_max = 0;
-	for (int i = 0; i<n; ++i){
-		sum += arr[i]; curr_max = max(curr_max, arr[i]);
-		mp[curr_max] = make_pair(i+1, sum);
+#include<bits/stdc++.h>
+using namespace std;
+// #define int long long int
+#define forn(i,a,b) for (int i=(int)(a);i<(int)(b);i++)
+#define rofn(i,a,b) for (int i=(int)(a);i>(int)(b);i--)
+#define all(x) (x).begin(),(x).end()
+#define ys cout << "YES";
+#define ns cout << "NO";
+#define ll long long
+#define ld long double
+#define llu long long unsigned
+#define si short int
+int tests(); int solve(){
+  //TODO tests()  solve() //
+    // !Start Here! */
+    
+    int n, q, tmp, cnt = 0; cin >> n;
+	map<ll, pair<ll, ll>> mp; ll pfx = 0;
+	forn(i, 0, n){
+		cin >> tmp;
+		if (mp.count(tmp)){
+			mp[tmp].first +=  1;
+		}
+		else mp[tmp].first = 1;
 	}
-	// cout << curr_max << endl;
-	// for (auto [i, j] : mp) cout << i << " = " << "{ " << j.first << ", " << j.second << "}\n";
-	for (auto qq : que){
-		if (qq < arr[0]){
-			cout << -1;
-		}
-		else if (qq >= curr_max){
-			cout << mp[curr_max].first << ' ' << mp[curr_max].second;
-		}
-		else{
-			auto it = mp.lower_bound(qq);
-			if (it -> first != qq) --it;
-			cout << it -> second.first << ' ' << it -> second.second;
-		}
-		cout << '\n';
+	for (auto [i, j] : mp){
+		// i is key || power, j is pfx
+		pfx += ((j.first) * i); cnt += j.first;
+		mp[i].first = pfx; mp[i].second = cnt;
 	}
-	return 0;
+  
+  	// for (auto [i, j] : mp) cout << i << ' ' << 
+  							// j.first << ' ' << 
+  							// j.second << '\n';
+							
+  	ll mn = mp.begin() -> first, mx = mp.rbegin() -> first;
+  	cin >> q; 
+  	// cout << "\nq= " << mn << '\n';
+  	while(q--){
+  		cin >> tmp;
+  		if (tmp < mn){ cout << -1 << '\n'; continue; }
+  		else if (tmp > mx){
+  			tmp = mx;
+  			cout << mp[tmp].second << ' ' << mp[tmp].first << '\n';
+  		}
+  		else{
+  			auto it = mp.lower_bound(tmp);
+  			if (it -> first != tmp) --it;
+  			cout << it -> second.second << ' ' << it -> second.first << '\n';
+  		}
+  		
+  	}
+    // !Stop Here! */
+    return 0;
 }
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    return solve();
+    return tests();
+}
+int tests(){int t; cin >> t;while(t--){solve();cout << '\n';}return 0;}
