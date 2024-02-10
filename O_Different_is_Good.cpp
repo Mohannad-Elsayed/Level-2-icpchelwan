fc4b26999cf122713bcfcdd0709458aa5d625a90
Mohannad-Elsayed <Mohannad_20230600@fci.helwan.edu.eg>
2024-02-10 03:55:13 +0200

    O ACC

---
+++ O_Different_is_Good.cpp
@@ -0,0 +1,47 @@
+// Problem: O - Different is Good
+// Contest: Virtual Judge - Week #2
+// URL: https://vjudge.net/contest/608666#problem/O
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+// By: Sakura Yamauchi
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+#include<bits/stdc++.h>
+using namespace std;
+// #define int long long int
+#define forn(i,a,b) for (int i=(int)(a);i<(int)(b);i++)
+#define rofn(i,a,b) for (int i=(int)(a);i>(int)(b);i--)
+#define all(x) (x).begin(),(x).end()
+#define ys cout << "YES";
+#define ns cout << "NO";
+#define ll long long
+#define ld long double
+#define llu long long 
+#define si short int
+int tests(); int solve(){
+  //TODO tests()  solve() //
+    // !Start Here! */
+    int n; cin >> n;
+    string s; cin >> s;
+    n = s.length();
+    if (n > 26) {cout << -1; return 0;}
+    int frq[26]{}, cnt = 0;
+    for (char ch : s){
+    	char x = ch-'a';
+    	if (!frq[x]){
+    		frq[x] = 1;
+    		cnt++;
+    	}
+    }
+    cout << n - cnt;
+  
+    // !Stop Here! */
+    return 0;
+}
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    return solve();
+    return tests();
+}
+int tests(){int t; cin >> t;while(t--){solve();cout << '\n';}return 0;}
