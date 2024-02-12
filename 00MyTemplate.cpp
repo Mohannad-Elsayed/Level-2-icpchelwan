8bd70bcf4c31a8448692dfaf6a6cf49b27c0eb6c
Mohannad-Elsayed <Mohannad_20230600@fci.helwan.edu.eg>
2024-02-12 20:24:05 +0200

    template updated

--- 00MyTemplate.cpp
+++ 00MyTemplate.cpp
@@ -22,7 +22,7 @@ int tests(); int solve(){
 }
 int main(){
     ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-    return solve(); // Comment this if problem has multiple tests
+    return solve(); // Commenxt this if problem has multiple tests
     return tests();
 }
 int tests(){int t; cin >> t;while(t--){solve();cout << '\n';}return 0;}
