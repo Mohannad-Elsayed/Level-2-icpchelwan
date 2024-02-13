36fb7ff85c4c71ad4d2c18d1f91c16f4441e8f63
Mohannad-Elsayed <Mohannad_20230600@fci.helwan.edu.eg>
2024-02-13 19:24:05 +0200

    .

--- cpeditor temp/W3 Sheet/Y.cpp
+++ cpeditor temp/W3 Sheet/Y.cpp
@@ -21,32 +21,18 @@ using namespace std;
 #define ld long double
 #define llu long long unsigned
 #define si short int
+
 int tests(); int solve(){
   //TODO tests()  solve() //
     // !Start Here! */
     int n, mxlen = INT_MIN; cin >> n;
-    vector<string> v;
-    map<string, string> mp;
-    forn(i, 0, n){
-    	string s; cin >> s;
-    	int leng = s.length();
-    	v.pb(s);
-    	mxlen = max(mxlen, leng);
+    vector<string> v(n);
+    for(auto &e : v) cin >> e;
+    sort(all(v));
+    for(auto s : v){
+    	cout << s << endl;
     }
-    forn(i, 0, n){
-    	// cout << "here: " << v[i] << ' ' << (mxlen-v[i].length()) << ' ' << mxlen << endl;
-    	int oldlen = v[i].length();
-    	string olds = v[i];
-    	if (v[i].length() < mxlen){
-    		forn(j, 0, (mxlen - oldlen)) v[i] += 'z';
-    	}
-    	mp[v[i]] = olds;
-    }
-    // cout << mxlen << '\n';
-    // for(auto s : v){
-    	// cout << s << endl;
-    // }
-    for (auto [x, y] : mp) cout << y;
+    // sort(all(v), fun);
     // !Stop Here! */
     return 0;
 }
