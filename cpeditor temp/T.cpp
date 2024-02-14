7049a44c788fd435abfe1a83d5785fa2e20379e8
Mohannad-Elsayed <Mohannad_20230600@fci.helwan.edu.eg>
2024-02-14 17:22:57 +0200

    T ACC, files renamed

---
+++ cpeditor temp/T.cpp
@@ -0,0 +1,37 @@
+// Problem: T - The Z sequence
+// Contest: Virtual Judge - Week #3
+// URL: https://vjudge.net/contest/609587#problem/T
+// Memory Limit: 64 MB
+// Time Limit: 700 ms
+// By: Sakura Yamauchi
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+#include<bits/stdc++.h>
+using namespace std;
+#define forn(i,a,b) for (int i=(int)(a);i<(int)(b);i++)
+#define rofn(i,a,b) for (int i=(int)(a);i>(int)(b);i--)
+#define all(x) (x).begin(),(x).end()
+#define ys cout << "YES";
+#define ns cout << "NO";
+#define F first
+#define S second
+#define pb push_back
+#define ll long long
+#define ld long double
+#define llu long long unsigned
+#define si short int
+int tests(); int solve(){
+  //TODO tests()  solve() //
+    // !Start Here! */
+    
+    
+    // !Stop Here! */
+    return 0;
+}
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    return solve(); // Comment this if problem has multiple tests
+    return tests();
+}
+int tests(){int t; cin >> t;while(t--){solve();cout << '\n';}return 0;}
