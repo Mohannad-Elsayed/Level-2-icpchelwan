ce8a004c8fa2659fbaedd7fa6c83c27c55d4a305
Mohannad-Elsayed <Mohannad_20230600@fci.helwan.edu.eg>
2024-02-19 15:22:28 +0200

    B ACC

---
+++ W4/B another solution.cpp
@@ -0,0 +1,47 @@
+// Problem: A - Printing some primes
+// Contest: Virtual Judge - Week #4
+// URL: https://vjudge.net/contest/610907#problem/A
+// Memory Limit: 1536 MB
+// Time Limit: 1325 ms
+// By: Sakura Yamauchi
+// When: 2024-02-19 13:05:58
+// Topic: Primes
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+#include<bits/stdc++.h>
+using namespace std;
+#define forn(i,a,b) for (int i=(int)(a);i<(int)(b);i++)
+#define rofn(i,a,b) for (int i=(int)(a);i>(int)(b);i--)
+#define all(x) (x).begin(),(x).end()
+#define ys cout << "YES";
+#define ns cout << "NO";
+#define F first
+#define S second
+#define pb push_back
+#define ll long long
+#define ld long double
+#define llu long long unsigned
+#define si short int
+bool prime(int x){
+	for (int i = 2; i*i <=x; ++i){
+		if (!(x%i)) return false;
+	}
+	return true;
+}
+int tests(); int solve(){
+  //TODO tests()  solve() //
+    // !Start Here! */
+    int n; cin >>n;
+    forn(i, n, 1e5+100){
+    	if (prime(i)) return (cout << i), 0;
+    }
+    // !Stop Here! */
+    return 0;
+}
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(0);
+    return solve(); // Comment this if problem has multiple tests
+    return tests();
+}
+int tests(){int t; cin >> t;while(t--){solve();cout << '\n';}return 0;}
