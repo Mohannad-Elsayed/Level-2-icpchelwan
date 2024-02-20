92a56a517a996bd735f90bd50e797360b5c62ed9
Mohannad-Elsayed <Mohannad_20230600@fci.helwan.edu.eg>
2024-02-20 19:40:04 +0200

    F ACC

--- W4/F.cpp
+++ W4/F.cpp
@@ -4,8 +4,8 @@
 // Memory Limit: 256 MB
 // Time Limit: 1000 ms
 // By: Sakura Yamauchi
-// When: 2024-02-19 15:21:15
-// Topic: 
+// When: 2024-02-20 18:59:05
+// Topic: LCM, GCD
 // 
 // Powered by CP Editor (https://cpeditor.org)
 
@@ -23,11 +23,34 @@ using namespace std;
 #define ld long double
 #define llu long long unsigned
 #define si short int
+int gcd(int a, int b){
+	if (!b) return a;
+	return gcd(b, a%b);
+}
+int lcm(int a, int b){
+	return ((a*b) / (gcd(a, b)));
+}
+bool isp(int x){
+	for (ll i = 2; i * i <= x; ++i){
+		if (!(x%i)) return false;
+	}
+	return true;
+}
 int tests(); int solve(){
   //TODO tests()  solve() //
     // !Start Here! */
-    
-    
+    int n; cin >> n;
+    if (!(n%2)) return (cout << n/2 << ' ' << n/2), 0;
+    // if (isp(n)) return (cout << n-2 << ' ' << 2), 0;
+    int mx = 1;
+    for (ll i = 2; i * i <= n; ++i){
+    	if (!(n%i)){
+    		if (n/i > mx) mx = n/i;
+    	}
+    }
+    (mx == 1) ? 
+    cout << n-1 << ' ' << 1 : 
+    cout << n-mx << ' ' << mx ;
     // !Stop Here! */
     return 0;
 }
@@ -36,4 +59,4 @@ int main(){
     // return solve(); // Comment this if problem has multiple tests
     return tests();
 }
-int tests(){int t; cin >> t;while(t--){solve();cout << '\n';}return 0;}
+int tests(){int t; cin >> t;while(t--){solve();cout << "\n "[(t==0)];}return 0;}
