533666793ca7bd8fd1c325c79d1125a8acb9196d
Mohannad-Elsayed <Mohannad_20230600@fci.helwan.edu.eg>
2024-02-20 07:30:03 +0200

    tmep updated

--- 00MyTemplate.cpp
+++ 00MyTemplate.cpp
@@ -25,4 +25,4 @@ int main(){
     return solve(); // Comment this if problem has multiple tests
     return tests();
 }
-int tests(){int t; cin >> t;while(t--){solve();cout << '\n';}return 0;}
+int tests(){int t; cin >> t;while(t--){solve();cout << "\n "[(t==0)];}return 0;}
