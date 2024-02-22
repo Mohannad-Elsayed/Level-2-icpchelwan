b3f07323ff13313fc9c71187e5e0c7b9a2206e6c
Mohannad-Elsayed <Mohannad_20230600@fci.helwan.edu.eg>
2024-02-22 13:43:59 +0200

    L ACC

---
+++ W4/L.cpp
@@ -0,0 +1,39 @@
+// Problem: L - EhAb AnD gCd
+// Contest: Virtual Judge - Week #4
+// URL: https://vjudge.net/contest/610907#problem/L
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+// By: Sakura Yamauchi
+// When: 2024-02-22 13:32:39
+// Topic: GCD, LCM, Divisors
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+#include<bits/stdc++.h>
+using namespace std;
+#define forn(i,a,b) for (int i=(int)(a);i<(int)(b);i++)
+#define rofn(i,a,b) for (int i=(int)(a);i>(int)(b);i--)
+#define all(x) (x).begin(),(x).end()
+#define ys cout << "YES";
+#define ns cout << "NO";
+#define F first
+#define S second
+#define pb push_back
+#define ll long long
+#define ld long double
+#define llu long long unsigned
+#define si short int
+int tests(); int solve(){
+  //TODO tests()  solve() //
+    // !Start Here! */
+    ll n; cin >> n;
+    cout << 1 << ' ' << n-1;
+    // !Stop Here! */
+    return 0;
+}
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(0);
+    // return solve(); // Comment this if problem has multiple tests
+    return tests();
+}
+int tests(){int t; cin >> t;while(t--){solve();cout << "\n "[(t==0)];}return 0;}
