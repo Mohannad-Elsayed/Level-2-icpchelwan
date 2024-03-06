575e128813e5a03c1af8a5dc6419440c77edd11e
Mohannad-Elsayed <Mohannad_20230600@fci.helwan.edu.eg>
2024-03-06 14:32:57 +0200

    temp updated

--- 00MyTemplate.cpp
+++ 00MyTemplate.cpp
@@ -3,6 +3,7 @@ using namespace std;
 #define forn(i,a,b) for (int i=(int)(a);i<(int)(b);i++)
 #define rofn(i,a,b) for (int i=(int)(a);i>(int)(b);i--)
 #define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
 #define each(a, x) for (auto &a : x)
 #define ys cout << "YES";
 #define ns cout << "NO";
@@ -13,7 +14,6 @@ using namespace std;
 #define ld long double
 #define llu long long unsigned
 #define si short int
-int test = 1;
 int solve(){
   //TODO tests()  solve() //
     // !Start Here! */
@@ -24,7 +24,9 @@ int solve(){
 }
 int main(){
     ios_base::sync_with_stdio(false);cin.tie(0);
+    int test = 1;
     // cin >> test;
+    // cout << "# " << test << '\n';
     while(test--){
       solve();
       cout << '\n';
