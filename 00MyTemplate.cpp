5bbbf21ffcc93c62593c35e227628700e77013b3
Mohannad-Elsayed <Mohannad_20230600@fci.helwan.edu.eg>
2024-03-21 13:31:10 +0200

    temp updated

--- 00MyTemplate.cpp
+++ 00MyTemplate.cpp
@@ -22,7 +22,7 @@ void solve(){
     
 }
 size_t main(){
-    ios_base::sync_with_stdio(false);cin.tie(0);
+    ios_base::sync_with_stdio(0);cin.tie(0);
     size_t t = 1;
     // cin >> test;
     forn(i, 1, t+1){
