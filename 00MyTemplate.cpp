6a32cdea1e0efe62666466bcb088129e9906235d
Mohannad-Elsayed <Mohannad_20230600@fci.helwan.edu.eg>
2024-03-21 13:30:32 +0200

    temp updated

--- 00MyTemplate.cpp
+++ 00MyTemplate.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define forn(i,a,b) for (int i=(int)(a);i<(int)(b);i++)
-#define rofn(i,a,b) for (int i=(int)(a);i>(int)(b);i--)
+#define forn(i,a,b) for(int i=(int)(a);i<(int)(b);i++)
+#define rofn(i,a,b) for(int i=(int)(a);i>(int)(b);i--)
 #define all(x) (x).begin(),(x).end()
 #define rall(x) (x).rbegin(),(x).rend()
 #define each(a, x) for (auto &a : x)
@@ -9,27 +9,26 @@ using namespace std;
 #define ns cout << "NO";
 #define F first
 #define S second
-#define pb push_back
+#define pb emplace_back
+#define pf emplace_front
+#define lwb lower_bound
+#define upb upper_bound
 typedef long long ll;
 typedef long double ld;
 typedef unsigned long long llu;
 typedef short int si;
 const llu MOD {1000000007};
-int solve(){
-  //TODO tests()  solve() //
-    // !Start Here! */
+void solve(){
     
-    
-    // !Stop Here! */
-    return 0;
 }
-int main(){
+size_t main(){
     ios_base::sync_with_stdio(false);cin.tie(0);
-    int test = 1;
+    size_t t = 1;
     // cin >> test;
-    forn(i, 1, test+1){
+    forn(i, 1, t+1){
     	// cout << "# " << i << '\n';
       	solve();
       	cout << '\n';
     }
+    return 0;
 }
